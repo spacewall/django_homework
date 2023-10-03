0ab330b909a213595267e28adfeb27e492338024
Alexander Godov <godovav@oiate.ru>
2023-10-03 15:53:15 +0300

    Edit models in the third task

--- databases/work_with_database/phones/models.py
+++ databases/work_with_database/phones/models.py
@@ -2,5 +2,11 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=21)
+    price = models.DecimalField(max_digits=5, decimal_places=0)
+    image = models.CharField(max_length=90)
+
+    release_date = models.DateField()
+    lte_exitsts = models.BooleanField()
+
+    slug = models.SlugField()

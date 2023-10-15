8fd8011370318f357d6dc420237e2deb226cf22e
Alexander Godov <godovav@oiate.ru>
2023-10-15 09:56:44 +0300

    Order by group

--- databases-2/orm_migrations/school/views.py
+++ databases-2/orm_migrations/school/views.py
@@ -7,7 +7,7 @@ from .models import Student
 def students_list(request):
     template = 'school/students_list.html'
     context = {
-        'object_list': Student.objects.all()
+        'object_list': Student.objects.all().order_by('group')
     }
 
     for item in context['object_list']:

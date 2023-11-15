33299e05f0878ab0420a1a6313e81fb7d8f04703
Alexander Godov <godovav@oiate.ru>
2023-11-15 20:12:05 +0300

    Final edition

--- drf-intro/smart_home/measurement/models.py
+++ drf-intro/smart_home/measurement/models.py
@@ -5,9 +5,15 @@ class Sensor(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=100, blank=True)
 
+    def __str__(self) -> str:
+        return f'{str(self.name)} ({str(self.id)})'
+
 
 class Measurement(models.Model):
     temperature = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
 
-    sensor = models.ForeignKey(Sensor, related_name='measurements', on_delete=models.CASCADE)
+    sensor = models.ForeignKey(Sensor, related_name='measurements', on_delete=models.CASCADE, related_query_name='sensor')
+
+    def __str__(self) -> str:
+        return f'{str(self.sensor.name)} ({str(self.sensor.id)})'
--- drf-intro/smart_home/measurement/views.py
+++ drf-intro/smart_home/measurement/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.response import Response
 
 from .models import Sensor, Measurement
 from .serializers import SensorDetailSerializer, MeasurementSerializer, SensorSerializer
@@ -12,6 +13,12 @@ class ListCreateSensorView(ListCreateAPIView):
 class RetrieveUpdateSensorView(RetrieveUpdateAPIView):
     queryset = Sensor.objects.all()
     serializer_class = SensorSerializer
+
+    def get_serializer_class(self):
+        if self.request.GET is None:
+            return SensorSerializer
+        
+        return SensorDetailSerializer
 
 
 class CreateMeasurementView(CreateAPIView):

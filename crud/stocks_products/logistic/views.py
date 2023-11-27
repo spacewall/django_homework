2110f04c9cde35dcc9241e9d5579cd3167e4a83c
Alexander Godov <godovav@oiate.ru>
2023-11-27 12:40:46 +0300

    The final edition

--- crud/stocks_products/logistic/views.py
+++ crud/stocks_products/logistic/views.py
@@ -15,4 +15,4 @@ class ProductViewSet(ModelViewSet):
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
+    filterset_fields = ['products']

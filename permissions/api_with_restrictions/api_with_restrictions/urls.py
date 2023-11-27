f9cb5314e66a096d89295232b22ccbfba03420e0
Alexander Godov <godovav@oiate.ru>
2023-11-27 14:33:30 +0300

    Register view set

--- permissions/api_with_restrictions/api_with_restrictions/urls.py
+++ permissions/api_with_restrictions/api_with_restrictions/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
-# TODO: подключите `AdvertisementViewSet`
+from advertisements.views import AdvertisementViewSet
 
+router = DefaultRouter()
+router.register(AdvertisementViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),

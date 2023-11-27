78a88a5b32363cc224a66ec45dfbc34d047cc588
Alexander Godov <godovav@oiate.ru>
2023-11-27 10:57:53 +0300

    Product model is correct

--- crud/stocks_products/logistic/views.py
+++ crud/stocks_products/logistic/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
 
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
@@ -7,7 +8,8 @@ from logistic.serializers import ProductSerializer, StockSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
 
 
 class StockViewSet(ModelViewSet):
